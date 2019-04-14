9c425f694c2685a2740fc40b5b69257001a642d1
imwy <iamwooyong@gmail.com>
2019-04-14 14:12:59 +0900

    웹뷰 page212

--- Web/ViewController.swift
+++ Web/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActiveIndicator: UIActivityIndicatorView!
+    
+    func loadWebPage(_ url: String){
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.load(myRequest)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadWebPage("http://naver.com")
     }
 
-
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoForword(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
